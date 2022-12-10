8922082e3bf872c6c2f94e1609fe835a543b3a32
Matthias <matthias-endler@gmx.net>
2022-12-10 22:01:44 +0100

    WIP: Use localStorage for votes

--- components/widgets/VoteWidget/VoteWidget.tsx
+++ components/widgets/VoteWidget/VoteWidget.tsx
@@ -1,9 +1,10 @@
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
 import { submitVote, votesFormatter } from 'utils/votes';
 import cn from 'classnames';
 import styles from './VoteWidget.module.css';
 import { LoadingDots } from '@components/elements';
 import { useToolVotesQuery } from './query';
+import { isApiTag } from 'utils/type-guards';
 export interface VoteWidgetProps {
     toolId: string;
     type?: 'primary' | 'secondary';
@@ -14,6 +15,14 @@ const VoteWidget: FC<VoteWidgetProps> = ({ toolId, type = 'primary' }) => {
 
     const { data, isLoading, isFetching, isRefetching, error, refetch } =
         useToolVotesQuery(toolId);
+
+    let votesStr = votesFormatter(data?.votes || 0);
+    let votes = data?.votes || 0;
+
+    useEffect(() => {
+        console.log('data', data);
+        votesStr = votesFormatter(data?.votes || 0);
+    }, [data, votes, votesStr]);
 
     if (isLoading || isFetching || isRefetching) {
         return (
@@ -26,22 +35,50 @@ const VoteWidget: FC<VoteWidgetProps> = ({ toolId, type = 'primary' }) => {
         return null;
     }
 
-    const votes = votesFormatter(data.votes || 0);
-
     const upVoteButtonClick = async () => {
-        const result = await submitVote(toolId, 1);
+        // Check local storage for vote
+        const localVote = localStorage.getItem(`vote-${toolId}`);
 
-        // TODO: Handle error
-        console.log(result);
-        refetch(); // FIXME: There is a delay until the vote recalculcation is triggered server side
+        if (localVote) {
+            // check if it was a downvote
+            if (localVote === 'downvote') {
+                votes += 2;
+                // remove downvote and add upvote
+                // await submitVote(toolId, 1);
+                localStorage.setItem(`vote-${toolId}`, 'upvote');
+            } else {
+                // already upvoted. Do nothing
+                return;
+            }
+        } else {
+            // No vote in local storage. Add upvote
+            // await submitVote(toolId, 1);
+            votes += 1;
+            localStorage.setItem(`vote-${toolId}`, 'upvote');
+        }
     };
 
     const downVoteButtonClick = async () => {
-        const result = await submitVote(toolId, -1);
+        // Check local storage for vote
+        const localVote = localStorage.getItem(`vote-${toolId}`);
 
-        // TODO: Handle error
-        console.log(result);
-        refetch(); // FIXME: There is a delay until the vote recalculcation is triggered server side
+        if (localVote) {
+            // check if we have an upvote
+            if (localVote === 'upvote') {
+                votes -= 2;
+                // remove upvote and add downvote
+                // await submitVote(toolId, -1);
+                localStorage.setItem(`vote-${toolId}`, 'downvote');
+            } else {
+                // already downvoted. Do nothing
+                return;
+            }
+        } else {
+            // No vote in local storage. Add downvote
+            // await submitVote(toolId, -1);
+            votes -= 1;
+            localStorage.setItem(`vote-${toolId}`, 'downvote');
+        }
     };
 
     return (
@@ -49,7 +86,7 @@ const VoteWidget: FC<VoteWidgetProps> = ({ toolId, type = 'primary' }) => {
             <button
                 className={styles.voteBtn}
                 onClick={upVoteButtonClick}></button>
-            <span className={styles.votes}>{votes}</span>
+            <span className={styles.votes}>{votesStr}</span>
             <button
                 className={cn(styles.voteBtn, styles.downvoteBtn)}
                 onClick={downVoteButtonClick}></button>
