4370c4a15cf82fcbf8ab756790f1935fa80d9a05
Matthias <matthias-endler@gmx.net>
2023-02-17 16:31:54 +0100

    Fix social graph tags
    MetaTags did not work. Maybe because headers are already rendered
    with MainHead? Anyway, I merged the two and the metatags
    started to show up.

--- components/core/MainHead/MainHead.tsx
+++ components/core/MainHead/MainHead.tsx
@@ -1,6 +1,5 @@
 import Head from 'next/head';
 import { FC } from 'react';
-import MetaTags from '../MetaTags/MetaTags';
 
 export interface MainHeadProps {
     title: string;
@@ -8,6 +7,8 @@ export interface MainHeadProps {
 }
 
 const MainHead: FC<MainHeadProps> = ({ title, description }) => {
+    const canonicalURL = '/';
+
     return (
         <Head>
             <meta charSet="utf-8" />
@@ -17,7 +18,25 @@ const MainHead: FC<MainHeadProps> = ({ title, description }) => {
             <title>{title}</title>
             <meta name="title" content={title} />
             <meta name="description" content={description} />
-            <MetaTags title={title} description={description} />
+
+            <meta property="og:title" content={title} />
+
+            {/* Open Graph / Facebook */}
+            <meta property="og:type" content="website" />
+            <meta property="og:url" content={canonicalURL} />
+            <meta property="og:title" content={title} />
+            <meta property="og:description" content={description} />
+            <meta property="og:image" content={'/assets/images/social.png'} />
+
+            {/* Twitter */}
+            <meta property="twitter:card" content="summary_large_image" />
+            <meta property="twitter:url" content={canonicalURL} />
+            <meta property="twitter:title" content={title} />
+            <meta property="twitter:description" content={description} />
+            <meta
+                property="twitter:image"
+                content={'/assets/images/social.png'}
+            />
         </Head>
     );
 };
