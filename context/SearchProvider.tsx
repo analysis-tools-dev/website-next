1d6a687cf7321096d3622b70a6f630bbace6abb2
GhadyK <ghady.kalaany@trivago.com>
2023-03-26 23:17:47 +0200

    Fix duplicate meta tags

--- context/SearchProvider.tsx
+++ context/SearchProvider.tsx
@@ -52,35 +52,16 @@ export interface SearchProviderProps {
 
 export const SearchProvider: FC<SearchProviderProps> = ({ children }) => {
     const router = useRouter();
-
     const [search, setSearch] = useState(router.query);
 
-    let title = 'Analysis Tools, Linters, Formatters and Code Quality Checkers';
-    const description =
-        'Find static code analysis tools and linters that can help you improve code quality. All tools are peer-reviewed by fellow developers to meet high standards.';
-
-    if (search.languages && search.languages.length > 0) {
-        // concatenate the languages array into a string, capitalize it and add to the beginning of the title
-        const languages = search.languages
-            .map(
-                (language) =>
-                    language.charAt(0).toUpperCase() + language.slice(1),
-            )
-            .join('/');
-        title = `${languages} ${title}`;
-    }
-
     return (
-        <>
-            <MainHead title={title} description={description} />
-            <SearchContext.Provider
-                value={{
-                    search,
-                    setSearch,
-                }}>
-                {children}
-            </SearchContext.Provider>
-        </>
+        <SearchContext.Provider
+            value={{
+                search,
+                setSearch,
+            }}>
+            {children}
+        </SearchContext.Provider>
     );
 };
 
