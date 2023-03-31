3f19439e08b9b7712ea2ad0096ecf52ff6b09730
Matthias <matthias-endler@gmx.net>
2023-03-31 16:11:57 +0200

    cleanup unused ToolsList

--- components/tools/listPage/index.ts
+++ components/tools/listPage/index.ts
@@ -1,6 +1,5 @@
 export * from './LanguageCard';
 export * from './ToolCard';
-export * from './ToolsList';
 export * from './AlternativeToolsList';
 export * from './ToolsSidebar';
 export * from './ListPageComponent';
