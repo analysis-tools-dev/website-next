cb5b0a1eedb242829a7cf7a8253cde0dc2288b6e
Matthias <matthias-endler@gmx.net>
2023-04-06 14:32:32 +0200

    wip expand tags

--- components/elements/TagList/TagList.tsx
+++ components/elements/TagList/TagList.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import Image from 'next/image';
 import cn from 'classnames';
 import styles from './TagList.module.css';
@@ -7,6 +7,8 @@ import { objectToQueryString } from 'utils/query';
 import { useRouterPush } from 'hooks';
 import classNames from 'classnames';
 import { tagIconPath } from 'utils/icons';
+
+const DEFAULT_TAG_LIMIT = 10;
 
 export interface TagListProps {
     languageTags?: string[];
@@ -17,6 +19,8 @@ export interface TagListProps {
 const TagList: FC<TagListProps> = ({ languageTags, otherTags, className }) => {
     const routerPush = useRouterPush();
     const { search, setSearch } = useSearchState();
+    const [showAll, setShowAll] = useState(false);
+
     const toggleLanguageTag = (event: any) => {
         const language = event?.target.innerText;
         if (Array.isArray(search.languages)) {
@@ -49,6 +53,7 @@ const TagList: FC<TagListProps> = ({ languageTags, otherTags, className }) => {
             },
         );
     };
+
     const toggleOtherTag = (event: any) => {
         const other = event?.target.innerText;
         if (Array.isArray(search.others)) {
@@ -78,19 +83,47 @@ const TagList: FC<TagListProps> = ({ languageTags, otherTags, className }) => {
         });
     };
 
-    // TODO: Show hide tags if over 10
+    const showMoreButton = (
+        <li className={classNames(styles.tag, styles.showMore)}>
+            <a onClick={() => setShowAll(true)}>more...</a>
+        </li>
+    );
+
     return (languageTags && languageTags.length) ||
         (otherTags && otherTags.length) ? (
         <ul className={cn(styles.tagList, className)}>
             {languageTags &&
-                languageTags.map((tag, index) => (
+                languageTags
+                    .slice(0, showAll ? undefined : DEFAULT_TAG_LIMIT)
+                    .map((tag, index) => (
+                        <li
+                            className={classNames(styles.tag, {
+                                [`${styles.highlight}`]:
+                                    search.languages?.includes(tag),
+                            })}
+                            key={`languageTag-${index}`}>
+                            <a onClick={toggleLanguageTag}>
+                                <Image
+                                    className={styles.tagIcon}
+                                    src={tagIconPath(tag)}
+                                    alt={tag}
+                                    width={15}
+                                    height={15}
+                                />
+                                {tag}
+                            </a>
+                        </li>
+                    ))}
+            {showAll &&
+                otherTags &&
+                otherTags.map((tag, index) => (
                     <li
                         className={classNames(styles.tag, {
                             [`${styles.highlight}`]:
-                                search.languages?.includes(tag),
+                                search.others?.includes(tag),
                         })}
-                        key={`languageTag-${index}`}>
-                        <a onClick={toggleLanguageTag}>
+                        key={`otherTag-${index}`}>
+                        <a onClick={toggleOtherTag}>
                             <Image
                                 className={styles.tagIcon}
                                 src={tagIconPath(tag)}
@@ -102,7 +135,12 @@ const TagList: FC<TagListProps> = ({ languageTags, otherTags, className }) => {
                         </a>
                     </li>
                 ))}
-            {otherTags &&
+            {!showAll &&
+                otherTags &&
+                otherTags.length > DEFAULT_TAG_LIMIT &&
+                showMoreButton}
+            {showAll &&
+                otherTags &&
                 otherTags.map((tag, index) => (
                     <li
                         className={classNames(styles.tag, {
