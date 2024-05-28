5ffb21af7401eb3ba810d8d62ed864fd8573c8e4
Matthias Endler <matthias@endler.dev>
2024-05-28 11:23:55 +0200

    Change heading effect to introduce a random typo every 10 seconds (#76)
    * Work on heading SEO
    
    * Introduce random typo in a loop

--- components/homepage/Intro/Intro.tsx
+++ components/homepage/Intro/Intro.tsx
@@ -1,4 +1,4 @@
-import { FC, useState } from 'react';
+import { FC, useEffect, useState } from 'react';
 import Image from 'next/image';
 import styles from './Intro.module.css';
 import { Heading, Text } from '@components/typography';
@@ -7,28 +7,69 @@ import { Wrapper } from '@components/layout';
 
 import homepageData from '@appdata/homepage.json';
 
-const Intro: FC = () => {
-    const [corrected, setCorrected] = useState(false);
-    const homepageIntro = homepageData.intro;
+const typoLookup: Record<string, string> = {
+    Because: 'Becuase',
+    Quality: 'Qualty',
+    Code: 'Cdoe',
+    Matters: 'Maters',
+};
 
-    const handleHeadingClick = () => {
-        setCorrected(true);
+const getRandomWordWithTypo = (text: string) => {
+    const words = text.split(' ');
+    const typoCandidates = words.filter((word) => typoLookup[word]);
+    const randomIndex = Math.floor(Math.random() * typoCandidates.length);
+    const typoWord = typoCandidates[randomIndex];
+
+    if (typoWord) {
+        return { original: typoWord, typo: typoLookup[typoWord] };
+    }
+    return null;
+};
+
+const Intro: FC = () => {
+    const homepageIntro = homepageData.intro;
+    const originalHeadline = 'Because Code Quality Matters';
+    const [headline, setHeadline] = useState(originalHeadline);
+    const [typoWord, setTypoWord] = useState<{
+        original: string;
+        typo: string;
+    } | null>(null);
+
+    const introduceTypo = () => {
+        const typo = getRandomWordWithTypo(originalHeadline);
+        if (typo) {
+            const newHeadline = originalHeadline.replace(
+                typo.original,
+                `<span class="${styles.squiggly} ${styles.typo}">${typo.typo}</span>`,
+            );
+            setHeadline(newHeadline);
+            setTypoWord(typo);
+        }
+    };
+
+    useEffect(() => {
+        introduceTypo();
+        const interval = setInterval(() => {
+            introduceTypo();
+        }, 10000);
+
+        return () => clearInterval(interval);
+    }, []);
+
+    const handleClick = () => {
+        if (typoWord) {
+            setHeadline(originalHeadline);
+            setTypoWord(null);
+        }
     };
 
     return (
         <Wrapper className={styles.introContainer}>
             <div className={styles.textContainer}>
-                <Heading
-                    level={1}
-                    className={styles.textHeading}
-                    onClick={handleHeadingClick}>
-                    Because Code{' '}
-                    {corrected ? (
-                        'Quality'
-                    ) : (
-                        <span className={styles.squiggly}>Qualty</span>
-                    )}{' '}
-                    Matters
+                <Heading level={1} className={styles.textHeading}>
+                    <span
+                        dangerouslySetInnerHTML={{ __html: headline }}
+                        onClick={handleClick}></span>
                 </Heading>
                 <Text
                     className={styles.textDescription}
